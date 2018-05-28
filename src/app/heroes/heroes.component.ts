6b6d81eb0a3661f279b0b9469e5d23c299e89643
ppavlova <ppavlova17@gmail.com>
2018-05-28 16:01:09 +1200

    7. Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   //hero = 'Windstorm';
   /**hero: Hero = {
@@ -17,7 +18,7 @@ export class HeroesComponent implements OnInit {
   //heroes = HEROES;
   heroes: Hero[];
   
-  selectedHero: Hero;
+  //selectedHero: Hero;
    
   constructor(private heroService: HeroService) { }
 
@@ -25,9 +26,9 @@ export class HeroesComponent implements OnInit {
 	this.getHeroes();
   }
   
-  onSelect(hero:Hero): void{
+  /**onSelect(hero:Hero): void{
 	this.selectedHero = hero;
-  }
+  }**/
   
   getHeroes(): void {
 	//this.heroes = this.heroService.getHeroes();
